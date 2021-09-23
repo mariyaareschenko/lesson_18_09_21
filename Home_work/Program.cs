a39d306be40438dd722400e48b94731f963519df
мария арещенко <mariya.areschenko@gmail.com>
2021-09-23 23:16:32 +0300

    сделала классную, лабораторную работу и домашнюю из файла,кроме А1.8

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -10,9 +10,88 @@ namespace Home_work
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Задание А1.1");
+            int sec = Convert.ToInt32(Console.ReadLine());
+            int hour = sec / 3600;
+            int minute = (sec - hour * 3600) / 60;
+            sec = sec - hour * 3600 - minute * 60;
+            Console.WriteLine("С начала суток прошло {0} часов {1} минут {2} секунд", hour, minute, sec);
 
+            Console.WriteLine("Задание А1.2");
+            int len_rectangle_1 = 543;
+            int len_rectangle_2 = 130;
+            int len_square = 130;
+            int count = (len_rectangle_1 / len_square) * (len_rectangle_2 / len_square);
+            Console.WriteLine("{0} квадрата можно отрезать", count);
 
+            Console.WriteLine("Задание 1.3");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (0 <= n & n <= 11)
+            {
+                int x = n + 1;
+                Console.WriteLine(x);
+            }
+            else if (n == 12)
+            {
+                int x = 1;
+                Console.WriteLine(x);
+            }
 
+            Console.WriteLine("Задание 1.4");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count_doz = num / 10;
+            int count_units = num % 10;
+            int sum = count_doz + count_units;
+            int product = count_doz * count_units;
+            Console.WriteLine("Число десятков: {0}\n Число единиц: {1}\n Сумма цифр: {2}\n Произведение цифр:{3}", count_doz, count_units, sum, product);
+
+            Console.WriteLine("Задание 1.5");
+            num = Convert.ToInt32(Console.ReadLine());
+            count_doz = num / 100;
+            int num_2 = num - count_doz * 100;
+            int finally_num = num_2 * 10 + count_doz;
+            Console.WriteLine("Полученное число: {0}", finally_num);
+
+            Console.WriteLine("Задание 1.6");
+            finally_num = 237;
+            count_units = finally_num / 100;
+            num_2 = finally_num - count_units * 100;
+            num = num_2 * 10 + count_units;
+            Console.WriteLine("Исходное число: {0}", num);
+
+            Console.WriteLine("Задание 1.7");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            while ((a1 + b1 + (a2 + b2) * 10) < 100)
+            {
+                int c1 = (a1 + b1) % 10;
+                int c2 = a2 + b2 + (a1 + b1) / 10;
+                Console.WriteLine("Полученные цифры: {0} {1}", c2, c1);
+                break;
+            }
+
+            Console.WriteLine("Задание 1.9");
+            bool A = true;
+            bool B = false;
+            bool C = false;
+            Console.WriteLine("A или B: {0}", A | B);
+            Console.WriteLine("A и B: {0}", A & B);
+            Console.WriteLine("B или C: {0}", B | C);
+
+            Console.WriteLine("Задание 1.10");
+            bool X = Convert.ToBoolean(Console.ReadLine());
+            bool Y = Convert.ToBoolean(Console.ReadLine());
+            bool Z = Convert.ToBoolean(Console.ReadLine());
+            bool a = !(X | Y) & (!X | !Z);
+            bool b = !(!X & Y) | (X | !Z);
+            bool c = X | !Y & !(X | !Z);
+            Console.WriteLine("a) {0}", a);
+            Console.WriteLine("б) {0}", b);
+            Console.WriteLine("в) {0}", c);
+
+            Console.ReadKey();
         }
     }
 }
--- Lab_2/Program.cs
+++ Lab_2/Program.cs
@@ -6,10 +6,48 @@ using System.Threading.Tasks;
 
 namespace Lab_2
 {
+    public enum AccountType { Actual, Savings }
+    public enum ВУЗ { КГУ, КАИ, КХТИ}
+    public struct employee
+    {
+        public string name;
+        public ВУЗ university;
+    }
+    public struct BankAccount
+    {
+        public long number;
+        public decimal balance;
+        public AccountType type;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Задание 3.1");
+            AccountType actualAccount;
+            AccountType savingAccount;
+            actualAccount = AccountType.Actual;
+            savingAccount = AccountType.Savings;
+            Console.WriteLine("Bank account type is {0}", actualAccount);
+            Console.WriteLine("Bank account type is {0}", savingAccount);
+
+            Console.WriteLine("Задание 3.2");
+            BankAccount bankAccount;
+            bankAccount.type = AccountType.Actual;
+            bankAccount.balance = (decimal)10000.00;
+            bankAccount.number = 123;
+            Console.WriteLine("Account number {0}", bankAccount.number);
+            Console.WriteLine("Account type {0}", bankAccount.type);
+            Console.WriteLine("Account balance {0}", bankAccount.balance);
+
+            Console.WriteLine("Домашнее задание 3.1");
+            employee information_employee;
+            information_employee.name = "Евгений";
+            information_employee.university = ВУЗ.КГУ;
+            Console.WriteLine("Имя сотрудника: {0}", information_employee.name);
+            Console.WriteLine("ВУЗ: {0}", information_employee.university);
+
+            Console.ReadKey();
         }
     }
 }
